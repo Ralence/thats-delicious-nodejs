8200d5a70be00edfb39bafe8c1c6e38e9d82077c
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-03-31 20:44:50 +0200

    feat: add add store page and form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,13 @@
 exports.homePage = (req, res, next) => {
   res.render("index");
 };
+
+exports.addStore = (req, res, next) => {
+  res.render("editStore", {
+    title: "Add Store"
+  });
+};
+
+exports.saveStore = (req, res, next) => {
+  res.json(req.body);
+};
