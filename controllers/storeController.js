96315cd34061ef23b92c85da3e20a0620ada24f4
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-04-03 12:23:25 +0200

    feat: query and render stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -18,3 +18,10 @@ exports.saveStore = async (req, res, next) => {
   req.flash("success", `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1 query the db for all stores
+  const stores = await Store.find();
+
+  res.render("stores", { title: "Stores", stores });
+};
