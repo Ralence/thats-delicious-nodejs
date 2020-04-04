4745fc3905d8cb9a11bc2ff12d25a8cafc8bc24e
Ranko Ostojic <ranko.ostojic@gmail.com>
2020-04-04 17:57:38 +0200

    feat: create edit store flow

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -7,7 +7,7 @@ exports.homePage = (req, res, next) => {
 
 exports.addStore = (req, res, next) => {
   res.render("editStore", {
-    title: "Add Store"
+    title: "Add Store",
   });
 };
 
@@ -24,4 +24,31 @@ exports.getStores = async (req, res) => {
   const stores = await Store.find();
 
   res.render("stores", { title: "Stores", stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1 find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2 confirm the are the owner of the store
+  // TODO
+  // 3 render out the edit form so user can edit their store
+
+  res.render("editStore", {
+    title: `Edit ${store.name}`,
+    store,
+  });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1 find and update the store
+  const store = await Store.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead ot the old one
+    runValidators: true,
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}</strong> <a href="/stores/${store.slug}">View Store</a>`
+  );
+  res.redirect(`/stores/${store.id}/edit`);
+  // 2 redirect and tell the user it worked
 };
